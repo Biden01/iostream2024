cc3da2b60f82953d78dfc68a032debc388322bf1
sabinabidan <erasylbidan@mail.ru>
2024-06-16 15:39:48 +0500

    fix errors

--- djangoLecturePage/views.py
+++ djangoLecturePage/views.py
@@ -7,28 +7,30 @@ redirect_page = 'main'
 
 
 def lecture(request):
-    if request.user.subscribe:
-        context = {
-            'lecture': Lecture.objects.values_list(),
-            'user': request.user
-        }
-        if request.method == "GET":
-            return render(request=request, template_name="lecture/task.html", context=context)
+    if request.user.is_anonymous == False:
+        if request.user.subscribe:
+            context = {
+                'lecture': Lecture.objects.values_list(),
+                'user': request.user
+            }
+            if request.method == "GET":
+                return render(request=request, template_name="lecture/task.html", context=context)
 
-        username = request.POST["username"]
-        password = request.POST["password"]
+            username = request.POST["username"]
+            password = request.POST["password"]
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("/lecture/")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/lecture/")
 
-        return render(request=request, template_name="lecture/task.html", context={"error": "Пайдаланушы атыңыз және/немесе құпия сөзіңіз дұрыс емес.", 'user':request.user})
+            return render(request=request, template_name="lecture/task.html", context={"error": "Пайдаланушы атыңыз және/немесе құпия сөзіңіз дұрыс емес.", 'user':request.user})
     else:
         return redirect(redirect_page)
 
 def print_input(request):
-    if request.user.subscribe:
-        return render(request=request, template_name='lecture/task1-1.html', context={})
+    if request.user.is_anonymous == False:
+        if request.user.subscribe:
+            return render(request=request, template_name='lecture/task1-1.html', context={})
     else:
         return redirect(redirect_page)

4dfd9f9346cfde1c6f08f437923f07b55de9db48
sabinabidan <erasylbidan@mail.ru>
2024-06-18 20:57:55 +0500

    fix errors

--- djangoLecturePage/views.py
+++ djangoLecturePage/views.py
@@ -7,34 +7,22 @@ redirect_page = 'main'
 
 
 def lecture(request):
-    if request.user.is_anonymous == False:
-        if request.user.subscribe:
-            context = {
-                'lecture': Lecture.objects.values_list(),
-                'user': request.user
-            }
-            if request.method == "GET":
-                return render(request=request, template_name="lecture/task.html", context=context)
+    context = {
+        'lecture': Lecture.objects.values_list(),
+        'user': request.user
+    }
+    if request.method == "GET":
+        return render(request=request, template_name="lecture/task.html", context=context)
 
-            username = request.POST["username"]
-            password = request.POST["password"]
+    username = request.POST["username"]
+    password = request.POST["password"]
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("/lecture/")
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect("/lecture/")
 
-            return render(request=request, template_name="lecture/task.html", context={"error": "Пайдаланушы атыңыз және/немесе құпия сөзіңіз дұрыс емес.", 'user':request.user})
-        else:
-            return redirect(redirect_page)
-    else:
-        return redirect(redirect_page)
+    return render(request=request, template_name="lecture/task.html", context={"error": "Пайдаланушы атыңыз және/немесе құпия сөзіңіз дұрыс емес.", 'user':request.user})
 
 def print_input(request):
-    if request.user.is_anonymous == False:
-        if request.user.subscribe:
-            return render(request=request, template_name='lecture/task1-1.html', context={})
-        else:
-            return redirect(redirect_page)
-    else:
-        return redirect(redirect_page)
+    return render(request=request, template_name='lecture/task1-1.html', context={})

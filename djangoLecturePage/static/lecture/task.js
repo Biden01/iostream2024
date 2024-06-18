cbdc509b1a4453c855d3206190abbba5b9a39f84
sabinabidan <erasylbidan@mail.ru>
2024-06-18 21:42:01 +0500

    fix errors

--- djangoLecturePage/static/lecture/task.js
+++ djangoLecturePage/static/lecture/task.js
@@ -1,5 +1,15 @@
-window.onload = function() {
-	setTimeout(function() {
-		window.scrollTo(0, 0);
-	}, 1);
-};
+// Предполагается, что у вас есть несколько фреймов с классом 'frame'
+var frames = document.querySelectorAll('iframe'); // или другой селектор для ваших фреймов
+var loadedFramesCount = 0;
+
+function onFrameLoad() {
+    loadedFramesCount++;
+    if (loadedFramesCount === frames.length) {
+        // Прокрутка наверх, когда все фреймы загружены
+        window.scrollTo(0, 0);
+    }
+}
+
+frames.forEach(function(frame) {
+    frame.addEventListener('load', onFrameLoad);
+});

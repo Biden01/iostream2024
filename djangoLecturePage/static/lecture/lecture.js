73022b070c2e2b83db03f89b169a2dc304c031ae
sabinabidan <erasylbidan@mail.ru>
2024-06-28 19:52:18 +0500

    new compiler

--- djangoLecturePage/static/lecture/lecture.js
+++ djangoLecturePage/static/lecture/lecture.js
@@ -1,18 +1,18 @@
-// Предполагается, что у вас есть несколько фреймов с классом 'frame'
-var frames = document.querySelectorAll('iframe'); // или другой селектор для ваших фреймов
-var loadedFramesCount = 0;
+// // Предполагается, что у вас есть несколько фреймов с классом 'frame'
+// var frames = document.querySelectorAll('iframe'); // или другой селектор для ваших фреймов
+// var loadedFramesCount = 0;
+//
+// function onFrameLoad() {
+//     loadedFramesCount++;
+//     if (loadedFramesCount === frames.length) {
+//         // Прокрутка наверх, когда все фреймы загружены
+//         window.scrollTo(0, 0);
+//     }
+// }
 
-function onFrameLoad() {
-    loadedFramesCount++;
-    if (loadedFramesCount === frames.length) {
-        // Прокрутка наверх, когда все фреймы загружены
-        window.scrollTo(0, 0);
-    }
-}
-
-frames.forEach(function(frame) {
-    frame.addEventListener('load', onFrameLoad);
-});
+// frames.forEach(function(frame) {
+//     frame.addEventListener('load', onFrameLoad);
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   const players = Array.from(document.querySelectorAll('video, audio')).map(p => new Plyr(p));

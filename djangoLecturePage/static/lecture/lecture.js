3f5c32a583f53bb53121d13a3b179949296bfc09
sabinabidan <erasylbidan@mail.ru>
2024-06-29 13:46:45 +0500

    add problems

--- djangoLecturePage/static/lecture/lecture.js
+++ djangoLecturePage/static/lecture/lecture.js
@@ -15,15 +15,31 @@
 // });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const players = Array.from(document.querySelectorAll('video, audio')).map(p => new Plyr(p));
+    const players = Array.from(document.querySelectorAll('video, audio')).map(p => new Plyr(p));
 });
 
-window.onload = function() {
+window.onload = function () {
     // Предположим, что у всех ссылок на видео есть класс 'video-link'
     var videoLinks = document.querySelectorAll("source");
 
-    videoLinks.forEach(function(link) {
+    videoLinks.forEach(function (link) {
         link.remove();
     });
 };
 
+
+var code;
+
+$(function() {
+    code = ace.edit("code");                      // создаем редактор из элемента с id="code"
+    code.getSession().setMode("ace/mode/html");   // говорим что код надо подсвечивать как Python код
+    code.setTheme("ace/theme/monokai");          // выбираем тему оформления для подсветки синтаксиса
+    code.getSession().setUseWorker(true);
+    code.setHighlightActiveLine(true); // Включите подсветку активной строки
+    code.setShowPrintMargin(true);               // опционально: убираем вертикальную границу в 80 сиволов
+    code.setOptions({
+    maxLines: Infinity,                       // опционально: масштабировать редактор вертикально по размеру кода
+    fontSize: "12pt",                         // опционально: размер шрифта ставим побольше
+});
+    code.$blockScrolling = Infinity;              // отключаем устаревшие, не поддерживаемые фишки редактора
+});
